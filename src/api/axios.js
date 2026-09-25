4dd0aef90286e9370884647fe8d3fc047f4f4060
Padmashree <padmashree1004@gmail.com>
2026-09-25 15:11:24 +0530

    Update StreakUp project

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://streak-up-frontend.onrender.com/",
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:5000",
 });
 
 export default api;
